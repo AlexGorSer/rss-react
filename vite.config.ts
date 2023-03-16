f3e849ab478fc624939fc046239f2c2e1a652cb0
AlexGorSer <taskekaminari@gmail.com>
2023-03-16 18:06:42 +0200

    feat: add new plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   test: {
     globals: true,
     environment: 'jsdom',
