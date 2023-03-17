a70e91d10bdcb17c7199cb1a3f6b8b788e90790c
AlexGorSer <taskekaminari@gmail.com>
2023-03-17 21:50:51 +0200

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import Rss from '../../assets/rss.svg';
+import Git from '../../assets/github.svg';
+import './Footer.scss';
 
 class Footer extends React.Component {
   render(): React.ReactNode {
-    return <footer>Footer</footer>;
+    return (
+      <footer className="footer__container">
+        <div className="git__logo__container">
+          <a
+            className="footer__link"
+            href="https://github.com/AlexGorSer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Git} alt="git-logo" />
+            <p>AlexGorSer</p>
+          </a>
+        </div>
+        <p>2023</p>
+        <div className="rss__logo__container">
+          <a
+            className="footer__link"
+            href="https://github.com/rolling-scopes-school/tasks/tree/master/react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="rss-logo" src={Rss} alt="rss-logo" />
+          </a>
+        </div>
+      </footer>
+    );
   }
 }
 export default Footer;
