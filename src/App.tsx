8ab064f10fba2fb658e846b057df8940164dbde8
AlexGorSer <taskekaminari@gmail.com>
2023-03-19 15:08:57 +0200

    fix: delete commints in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,6 @@ import PageNotFound from './components/404/PageNotFound';
 import MainLayout from './components/layout/MainLayout';
 import Main from './components/Main/Main';
 
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-//           <img src="/vite.svg" className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-//     </div>
-//   );
-// }
-
-// type ContentPropApp = { props?: string };
-
 class App extends React.Component {
   render(): React.ReactNode {
     return (
